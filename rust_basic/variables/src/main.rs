f984f687c397c52380ca30b51854738a2567729e
sugimochi <tashinoki.1019@gmail.com>
2025-07-30 09:19:20 +0900

    変数と可変性

--- rust_basic/variables/src/main.rs
+++ rust_basic/variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let x = 5;
     let x = x + 1;
@@ -24,8 +26,50 @@ fn main() {
     println!("The value of floored is: {}", floored);
     let remainder = 43 % 5;
     println!("The value of remainder is: {}", remainder);
-    let t = ture;
-    let f:bool = false;
 
-    
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (_x, y, _z) = tup;
+    println!("The value of y is: {}", y);
+
+    let x: (i32, f64, u8) = (400, 3.2, 2);
+    println!("{}, {}, {}", x.0, x.1, x.2);
+
+    let _months = ["January", "February", "March", "April", "May", "June", "July",
+                 "August", "September", "October", "November", "December"];
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("The value of the first element is: {}", a[0]);
+    println!("The value of the second element is: {}", a[1]);
+    println!("The value of the third element is: {}", a[2]);
+    println!("The value of the fourth element is: {}", a[3]);
+    println!("The value of the fifth element is: {}", a[4]);
+
+    let a: [i32; 5] = [3; 5];
+    println!("The value of the first element is: {}", a[0]);
+    println!("The value of the second element is: {}", a[1]);
+    println!("The value of the third element is: {}", a[2]);
+    println!("The value of the fourth element is: {}", a[3]);
+    println!("The value of the fifth element is: {}", a[4]);
+
+    let a = [1, 2, 3, 4, 5];
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
+
+
 }
