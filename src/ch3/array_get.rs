872de325552e61afdffdb3bade7a331ebaa26af2
sugimochi <tashinoki.1019@gmail.com>
2025-09-03 09:36:48 +0900

    get_array

---
+++ src/ch3/array_get.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    println!("{}", month[0]);
+    println!("{}", month[11]);
+}
